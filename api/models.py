548392f9d55a82ad8a9dd9ae5b48259591fe37bb
JNelson <nelson.jimmy@gmail.com>
2018-08-27 15:05:36 -0600

    Adds meal model and meal serializer

--- api/models.py
+++ api/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Food(models.Model):
-    """This class represents the food model."""
     name = models.CharField(max_length=255, blank=False, unique=True)
     calories = models.IntegerField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    
+
+class Meal(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    foods = models.ManyToManyField(Food)
